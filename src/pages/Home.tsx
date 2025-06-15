cb9369440a0dc7cc51dcf9a6c7c034b84873ab5b
txngjr <txngjr@gmail.com>
2025-06-15 12:19:26 +0700

    add text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ function Home() {
   return (
     <>
       <h1 style={{ fontFamily: 'cursive' }}>สวัสดี</h1>
-      <h1 style={{ fontFamily: 'Noto Sans Thai' }}>สวัสดี</h1>
+      <h1 style={{ fontFamily: 'Noto Sans Thai' }}>สวัสดี0077546456234</h1>
       <h1 style={{ fontFamily: 'sans-serif' }}>สวัสดี</h1>
     </>
   );
